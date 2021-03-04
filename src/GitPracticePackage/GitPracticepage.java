f5263e6278b59ac631f9401672f1cd3e206b291f
falconsTek <esan2035@gmail.com>
2021-03-04 12:38:25 -0700

    changes to file

--- src/GitPracticePackage/GitPracticepage.java
+++ src/GitPracticePackage/GitPracticepage.java
@@ -2,9 +2,14 @@ package GitPracticePackage;
 
 public class GitPracticepage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	
+		String name = "Mustafa";
+		String mame = "Mama Esan";
+		
+		
+		
 
 	}
 
-}
+
